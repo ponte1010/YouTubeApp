09c5eafee7d6def283e18e4510819fdf6a54fc75
ponte1010 <t.miwa.528@nitech.jp>
2020-11-16 23:45:35 +0900

    Add: アイコンを追加する

--- lib/main.dart
+++ lib/main.dart
@@ -8,9 +8,24 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Colors.white,
+      ),
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('YouTube App'),
+          centerTitle: false,
+          leading: Icon(Icons.videocam),
+          title: const Text(
+            'YouTube App',
+          ),
+          actions: <Widget>[
+            FlatButton(
+              child: Icon(Icons.search),
+            ),
+            FlatButton(
+              child: Icon(Icons.more_vert),
+            ),
+          ],
         ),
         body: Container(),
       ),
