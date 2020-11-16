fb4efa5b3a831cdcbfb6c3934abf4405186337b0
ponte1010 <t.miwa.528@nitech.jp>
2020-11-16 23:11:54 +0900

    third commit

--- lib/main.dart
+++ lib/main.dart
@@ -8,34 +8,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({
-  Key key,
-  }) : super(key: key);
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('YouTube App'),
-      ),
-      body: Center(
-        child: Container(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('YouTube App'),
+        ),
+        body: Container(),
       ),
     );
   }
